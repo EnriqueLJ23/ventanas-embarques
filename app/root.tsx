2b54e5ad2badee98ff9e5c915a3229e84c920f4d
Enrique Luna <enrique.jasso@tq1.com.mx>
2026-07-14 14:27:32 -0600

    titulo

--- app/root.tsx
+++ app/root.tsx
@@ -28,6 +28,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
+        <title>Embarques</title>
         <Meta />
         <Links />
       </head>
